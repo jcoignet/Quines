abfb642ea7a5f5e55754b6d24c1b05cb54a3dcd6
Julian COIGNET <julian.coignet@gmail.com>
2016-03-22 19:21:17 +0100

    Colleen in rust

---
+++ Colleen.rs
@@ -0,0 +1,6 @@
+// Comment
+fn main(){
+// Comment 2
+let s = "// Comment{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}";
+print!("// Comment{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}", 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);
+}
