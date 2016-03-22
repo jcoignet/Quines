cc84d4b1f98e3947a34977b780737fca89f9c7f4
Julian COIGNET <julian.coignet@gmail.com>
2016-03-22 23:32:02 +0100

    improved colleen.rs

--- Colleen/colleen.rs
+++ Colleen/colleen.rs
@@ -2,6 +2,34 @@
 fn lol(){}
 fn main(){
 // Comment 2
-let s = "// Comment{}fn lol(){}{}{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 125 as char, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}";
-print!("// Comment{}fn lol(){}{}{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 125 as char, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}", 10 as char, 123 as char, 125 as char, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);
+lol();
+let s = "// Comment
+fn lol(){}{}
+fn main(){}
+// Comment 2
+lol();
+let s = {}{}{};
+print!({}{}{},
+123 as char,125 as char,
+123 as char,
+34 as char,s,34 as char,
+34 as char,s,34 as char,
+10 as char,125 as char,10 as char);{}{}{}";
+print!("// Comment
+fn lol(){}{}
+fn main(){}
+// Comment 2
+lol();
+let s = {}{}{};
+print!({}{}{},
+123 as char,125 as char,
+123 as char,
+34 as char,s,34 as char,
+34 as char,s,34 as char,
+10 as char,125 as char,10 as char);{}{}{}",
+123 as char,125 as char,
+123 as char,
+34 as char,s,34 as char,
+34 as char,s,34 as char,
+10 as char,125 as char,10 as char);
 }
