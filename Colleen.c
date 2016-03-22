890614747767f6bc2da255092e01687c8becb0bf
Julian COIGNET <julian.coignet@gmail.com>
2016-03-22 18:17:23 +0100

    Colleen OK

--- Colleen.c
+++ Colleen.c
@@ -6,6 +6,6 @@ int main(){
 /*
 	win
 */
-	char *s = "#include <stdio.h>%c%c%c%c%cok%c%c%c%cint main(){%c%c%c%c%cwin%c%c%c%cchar *s = %c%s%c;printf(s,10,47,42,10,9,10,42,47,10,10,47,42,10,9,10,42,47,10,34,9,34,10);%c}";
-	printf(s, 10, 47, 42, 10, 9, 10, 42, 47, 10, 10, 47, 42, 10, 9, 10, 42, 47, 10, 34, 9, 34, 10);
+	char *s = "#include <stdio.h>%c%c*%c%cok%c*%c%cint main(){%c%c*%c%cwin%c*%c%c%cchar *s = %c%s%c;%c%cprintf(s,10,47,10,9,10,47,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);%c}";
+	printf(s,10,47,10,9,10,47,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);
 }
