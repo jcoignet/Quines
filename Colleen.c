543fab202f1653f07994422d71cffccbc4009f6d
Julian COIGNET <julian.coignet@gmail.com>
2016-03-22 19:47:09 +0100

    Colleen.c: add empty fn

--- Colleen.c
+++ Colleen.c
@@ -2,10 +2,12 @@
 /*
 	ok
 */
+void lol(){
+}
 int main(){
 /*
 	win
 */
-	char *s = "#include <stdio.h>%c%c*%c%cok%c*%c%cint main(){%c%c*%c%cwin%c*%c%c%cchar *s = %c%s%c;%c%cprintf(s,10,47,10,9,10,47,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);%c}";
-	printf(s,10,47,10,9,10,47,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);
+	char *s = "#include <stdio.h>%c%c*%c%cok%c*%c%cvoid lol(){%c}%cint main(){%c%c*%c%cwin%c*%c%c%cchar *s = %c%s%c;%c%cprintf(s,10,47,10,9,10,47,10,10,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);%c}";
+	printf(s,10,47,10,9,10,47,10,10,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);
 }
