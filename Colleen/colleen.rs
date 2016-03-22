7c614007bd4d3b03e39ebedf9940e7b88349155a
Julian COIGNET <julian.coignet@gmail.com>
2016-03-22 20:18:35 +0100

    fix colleen.rs empty fun

--- Colleen/colleen.rs
+++ Colleen/colleen.rs
@@ -1,6 +1,7 @@
 // Comment
+fn lol(){}
 fn main(){
 // Comment 2
-let s = "// Comment{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}";
-print!("// Comment{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}", 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);
+let s = "// Comment{}fn lol(){}{}{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 125 as char, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}";
+print!("// Comment{}fn lol(){}{}{}fn main(){}{}// Comment 2{}let s = {}{}{};{}print!({}{}{}, 10 as char, 123 as char, 125 as char, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);{}{}{}", 10 as char, 123 as char, 125 as char, 10 as char, 123 as char, 10 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 34 as char, s, 34 as char, 10 as char, 125 as char, 10 as char);
 }
