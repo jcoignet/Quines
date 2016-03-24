c5e0cde0d1c4d5ed5b4236e6ce19bece68053246
Julian COIGNET <julian.coignet@gmail.com>
2016-03-24 18:36:27 +0100

    Sully.rs wont decrement at 1st exe

--- Sully/Sully.rs
+++ Sully/Sully.rs
@@ -1,3 +1,4 @@
+use std::env::args;
 use std::fs::File;
 use std::process::Command;
 use std::io::prelude::*;
@@ -5,7 +6,8 @@ macro_rules! son_name {
 () => ("Sully_")
 }
 macro_rules! son_soul {
-() => ("use std::fs::File;
+() => ("use std::env::args;
+use std::fs::File;
 use std::process::Command;
 use std::io::prelude::*;
 macro_rules! son_name {}
@@ -16,22 +18,21 @@ macro_rules! son_soul {}
 {}
 fn main(){}
 let mut i = {};
-if i > 0
-{}
-i -= 1;
+if i == 0 {}std::process::exit(0);{}
+else if args().filter(|x| x == {}./SullyRust{}).count() == 0 {}i -= 1;{}
 let f = File::create(format!({}{}{}{}{}.rs{}, son_name!(), i));
 assert!(f.unwrap().write_fmt(format_args!(son_soul!(),
 123 as char,34 as char,son_name!(),34 as char,125 as char,
 123 as char,34 as char,son_soul!(),34 as char,125 as char,
 123 as char,
 i,
-123 as char,
+123 as char,125 as char,
+34 as char,34 as char,123 as char,125 as char,
 34 as char,123 as char,125 as char,123 as char,125 as char,34 as char,
 34 as char,34 as char,34 as char,123 as char,125 as char,123 as char,125 as char,34 as char,
 123 as char,34 as char,123 as char,125 as char,34 as char,125 as char,
 34 as char,123 as char,125 as char,123 as char,125 as char,34 as char,
 123 as char,34 as char,123 as char,125 as char,34 as char,125 as char,
-125 as char,
 125 as char
 )).is_ok());
 Command::new({}rustc{}).arg(format!({}{}{}{}{}.rs{},son_name!(),i)).output()
@@ -39,32 +40,29 @@ Command::new({}rustc{}).arg(format!({}{}{}{}{}.rs{},son_name!(),i)).output()
 Command::new(format!({}./{}{}{}{}{},son_name!(),i)).output()
 .unwrap_or_else(|e|{}panic!({}{}{}{},e){});
 {}
-{}
 ")
 }
 fn main(){
 let mut i = 5;
-if i > 0
-{
-i -= 1;
+if i == 0 {std::process::exit(0);}
+else if args().filter(|x| x == "./SullyRust").count() == 0 {i -= 1;}
 let f = File::create(format!("{}{}.rs", son_name!(), i));
 assert!(f.unwrap().write_fmt(format_args!(son_soul!(),
 123 as char,34 as char,son_name!(),34 as char,125 as char,
 123 as char,34 as char,son_soul!(),34 as char,125 as char,
 123 as char,
 i,
-123 as char,
+123 as char,125 as char,
+34 as char,34 as char,123 as char,125 as char,
 34 as char,123 as char,125 as char,123 as char,125 as char,34 as char,
 34 as char,34 as char,34 as char,123 as char,125 as char,123 as char,125 as char,34 as char,
 123 as char,34 as char,123 as char,125 as char,34 as char,125 as char,
 34 as char,123 as char,125 as char,123 as char,125 as char,34 as char,
 123 as char,34 as char,123 as char,125 as char,34 as char,125 as char,
-125 as char,
 125 as char
 )).is_ok());
 Command::new("rustc").arg(format!("{}{}.rs",son_name!(),i)).output()
 .unwrap_or_else(|e|{panic!("{}",e)});
 Command::new(format!("./{}{}",son_name!(),i)).output()
 .unwrap_or_else(|e|{panic!("{}",e)});
-}
 }
