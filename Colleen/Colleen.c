a90993442a902b558df7797baecd2e916c06de71
Julian COIGNET <julian.coignet@gmail.com>
2016-03-24 17:55:47 +0100

    Colleen: add call to fn

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -8,6 +8,7 @@ int main(){
 /*
 	win
 */
-	char *s = "#include <stdio.h>%c%c*%c%cok%c*%c%cvoid lol(){%c}%cint main(){%c%c*%c%cwin%c*%c%c%cchar *s = %c%s%c;%c%cprintf(s,10,47,10,9,10,47,10,10,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);%c}";
-	printf(s,10,47,10,9,10,47,10,10,10,10,47,10,9,10,47,10,9,34,s,34,10,9,10);
+	lol();
+	char *s = "#include <stdio.h>%c%c*%c%cok%c*%c%cvoid lol(){%c}%cint main(){%c%c*%c%cwin%c*%c%c%clol();%c%cchar *s = %c%s%c;%c%cprintf(s,10,47,10,9,10,47,10,10,10,10,47,10,9,10,47,10,9,10,9,34,s,34,10,9,10);%c}";
+	printf(s,10,47,10,9,10,47,10,10,10,10,47,10,9,10,47,10,9,10,9,34,s,34,10,9,10);
 }
